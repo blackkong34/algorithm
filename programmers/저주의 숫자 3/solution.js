e39381f935c52dd8fcbd6396b4211c913401feab
blackkong34 <107909808+blackkong34@users.noreply.github.com>
2023-02-28 14:25:10 +0900

    updated at 2023. 2. 28. 오후 2:25:10

--- programmers/저주의 숫자 3/solution.js
+++ programmers/저주의 숫자 3/solution.js
@@ -1,8 +1,6 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120871?language=javascript
 
 function solution(n) {
-    // let arr = Array(200).fill(1).map((v,idx) =>  (v * idx) + "");
-    // return arr.filter(v => !v.includes(3) && v % 3 !== 0)[n-1]*1;
-    let result = 0;
-     return Array.from({ length: n }).length
+    let arr = Array(200).fill(1).map((v,idx) =>  (v * idx));
+    return arr.filter(v => !((v+"").includes(3) || v % 3 === 0))[n-1]
 }
