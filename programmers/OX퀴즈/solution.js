dca0d86538281aa29493463c860e4bbbb26a1bd3
blackkong34 <107909808+blackkong34@users.noreply.github.com>
2023-03-23 11:40:37 +0900

    updated at 2023. 3. 23. 오전 11:40:37

--- programmers/OX퀴즈/solution.js
+++ programmers/OX퀴즈/solution.js
@@ -4,6 +4,8 @@ function solution(quiz) {
     var answer = [];    
     return quiz.map(v => {
         let [q, ans] = v.split(" = ");
-        return eval(q) == ans ? "O" : "X"
+        let question = new Function(`return ${q}`);
+        return question() == ans ? "O" : "X";
+        // return eval(q) == ans ? "O" : "X"
     })
 }
