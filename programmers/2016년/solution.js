836af5192bf2ce4e1190fe14a06450a005614675
blackkong34 <107909808+blackkong34@users.noreply.github.com>
2023-04-04 10:01:31 +0900

    updated at 2023. 4. 4. 오전 10:01:31

--- programmers/2016년/solution.js
+++ programmers/2016년/solution.js
@@ -1,16 +1,16 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12901?language=javascript
 
 function solution(a, b) {
-//     let days = ["THU","FRI","SAT","SUN","MON","TUE","WED"];
-//     let months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-//     let sum = b;
+    let days = ["THU","FRI","SAT","SUN","MON","TUE","WED"];
+    let months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let sum = b;
 
-//     for(let i = 0;  i < a-1; i++) {
-//         sum += months[i];
-//     }
+    for(let i = 0;  i < a-1; i++) {
+        sum += months[i];
+    }
 
-//     return days[sum % 7];
-    
-    var date = new Date(2016, a-1, b);
-    return date.toString().slice(0,3).toUpperCase();
+    return days[sum % 7];
+//     
+    // var date = new Date(2016, a-1, b);
+    // return date.toString().slice(0,3).toUpperCase();
 }
