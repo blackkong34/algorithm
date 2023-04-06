c31c9fec5e493fff37f7186ece8a813cde583852
blackkong34 <107909808+blackkong34@users.noreply.github.com>
2023-04-06 12:59:28 +0900

    updated at 2023. 4. 6. 오후 12:59:29

--- programmers/약수의 합/solution.js
+++ programmers/약수의 합/solution.js
@@ -2,10 +2,10 @@
 
 function solution(n) {
     var answer = 0;
-    // return Array.from({length : n}, (_, idx) => n % (idx + 1) === 0 ? answer += (idx + 1) : answer)[n-1];
+    return Array.from({length : n}, (_, idx) => n % (idx + 1) === 0 ? answer += (idx + 1) : answer)[n-1];
     
-    for(let i = 1; i <= n; i++) {
-        n % i === 0 ? answer += i : answer
-    }
-    return answer
-}
+//     for(let i = 1; i <= n; i++) {
+//         n % i === 0 ? answer += i : answer
+//     }
+//     return answer
+// }
