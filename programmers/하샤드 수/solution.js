9415fae83ad64591c0f7663aa8128a08cd27da80
blackkong34 <107909808+blackkong34@users.noreply.github.com>
2023-04-07 17:36:01 +0900

    updated at 2023. 4. 7. 오후 5:36:01

--- programmers/하샤드 수/solution.js
+++ programmers/하샤드 수/solution.js
@@ -1,5 +1,5 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12947
 
 function solution(x) {
-    return x % [...x.toString()].reduce((a, c) => a + Number(c),0) === 0
+    return !(x % [...x.toString()].reduce((a, c) => a + Number(c),0));
 }
