9b54af5bb170e7f75fb0be4647d8858427ebfbb4
blackkong34 <107909808+blackkong34@users.noreply.github.com>
2023-04-07 22:33:27 +0900

    uploaded at 2023. 4. 7. 오후 10:33:26

---
+++ programmers/핸드폰 번호 가리기/solution.js
@@ -0,0 +1,15 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12948
+
+function solution(phone_number) {
+    var answer = '';
+    
+    for(let i = 0; i < phone_number.length; i++) {
+       answer += i < phone_number.length - 4 ? "*" : phone_number[i];
+    }
+    return answer;
+    
+    // phone_number = [...phone_number];
+    // let length = phone_number.length - 4;
+    // let removed = phone_number.splice(0, length, "*".repeat(length))
+    // return phone_number.join("")
+}
