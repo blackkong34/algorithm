be506f693d8d71dbe8b2ce71941cfaefe1de221d
blackkong34 <107909808+blackkong34@users.noreply.github.com>
2023-04-07 18:08:09 +0900

    updated at 2023. 4. 7. 오후 6:08:09

--- programmers/콜라츠 추측/solution.js
+++ programmers/콜라츠 추측/solution.js
@@ -1,18 +1,39 @@
-// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12943#
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12943?language=javascript
 
 function solution(num) {
     var answer = 0;
     
-    while(num) {
-        if(num === 1) {
-            break;
-        }
-        else {
-            num = num % 2 ? (num * 3) + 1 : num / 2;
-            answer++;   
-        }
+//     while(num) {
+//         if(num === 1) {
+//             break;
+//         }
+//         else {
+//             num = num % 2 ? (num * 3) + 1 : num / 2;
+//             answer++;   
+//         }
       
-    }
+//     }
     
-    return answer >= 500 ? -1 : answer;
+//     return answer >= 500 ? -1 : answer;    
+    
+    //     while(num < 500) {
+    //         if(num === 1) {
+    //             return answer;
+    //             }
+    //         answer++;  
+    //         num = num % 2 ? (num * 3) + 1 : num / 2;              
+    //     }
+    // return -1;
+    
+       while (answer < 500) {
+        if (num === 1) {
+            return answer;
+        }
+        answer++;
+        num = num % 2 === 0 ? num /2 : num *3 +1;
+    }
+
+    return -1;
+    
+    
 }
