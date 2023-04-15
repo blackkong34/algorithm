c117b5c82735d432cf74f45c987eebfad313da99
blackkong34 <107909808+blackkong34@users.noreply.github.com>
2023-04-15 12:40:33 +0900

    updated at 2023. 4. 15. 오후 12:40:33

--- programmers/약수의 개수와 덧셈/solution.js
+++ programmers/약수의 개수와 덧셈/solution.js
@@ -2,20 +2,21 @@
 
 function solution(left, right) {
     var answer = 0;
-    let divisor = 0;
-    for(let i = left; i <= right; i++) {
-        for(let j = 1; j <= Math.sqrt(i); j++) {
-            if(Math.pow(j, 2) !== i) {
-                i % j ? divisor : divisor += 2;
-            } else i % j ? divisor : divisor++;        
-        }
-        divisor % 2 ? answer -= i : answer += i;
-        divisor = 0;
+//     let divisor = 0;
+//     for(let i = left; i <= right; i++) {
+//         for(let j = 1; j <= Math.sqrt(i); j++) {
+//             if(Math.pow(j, 2) !== i) {
+//                 i % j ? divisor : divisor += 2;
+//             } else i % j ? divisor : divisor++;        
+//         }
+//         divisor % 2 ? answer -= i : answer += i;
+//         divisor = 0;
+//     }
+//     return answer;
+// }
+
+      for(let i = left; i <= right; i++) {
+        Number.isInteger(Math.sqrt(i)) ? answer -= i: answer +=i
     }
     return answer;
 }
-
-    //   for(let i = left; i <= right; i++) {
-    //     Number.isInteger(Math.sqrt(i)) ? answer -= i: answer +=i
-    // }
-    // return answer;
