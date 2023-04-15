d658c896ecd07217a36e315c1ce90971d0e569a7
blackkong34 <107909808+blackkong34@users.noreply.github.com>
2023-04-15 11:45:53 +0900

    uploaded at 2023. 4. 15. 오전 11:45:52

---
+++ programmers/약수의 개수와 덧셈/solution.js
@@ -0,0 +1,16 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/77884
+
+function solution(left, right) {
+    var answer = 0;
+    let divisor = 0;
+    for(let i = left; i <= right; i++) {
+        for(let j = 1; j <= Math.sqrt(i); j++) {
+            if(Math.pow(j, 2) !== i) {
+                i % j ? divisor : divisor += 2;
+            } else i % j ? divisor : divisor++;        
+        }
+        divisor % 2 ? answer -= i : answer += i;
+        divisor = 0;
+    }
+    return answer;
+}
