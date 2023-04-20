895e3376b83af3d01551b888a892f25be2dd10c3
blackkong34 <107909808+blackkong34@users.noreply.github.com>
2023-04-20 19:40:39 +0900

    uploaded at 2023. 4. 20. 오후 7:40:45

---
+++ programmers/수 조작하기 1/solution.js
@@ -0,0 +1,15 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181926
+
+function solution(n, control) {
+    let a = {
+        "w" : "+1",
+        "s" : "-1", 
+        "d" : "+10", 
+        "a" : "-10", 
+    }
+    
+    for(let i = 0; i <= control.length-1; i++) {
+        n += a[control[i]];
+    }
+    return eval(n);
+}
