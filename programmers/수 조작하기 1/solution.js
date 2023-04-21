1842471cb2b0d15c566bbd94234616f487734c18
blackkong34 <107909808+blackkong34@users.noreply.github.com>
2023-04-21 23:03:17 +0900

    updated at 2023. 4. 21. 오후 11:03:17

--- programmers/수 조작하기 1/solution.js
+++ programmers/수 조작하기 1/solution.js
@@ -2,14 +2,14 @@
 
 function solution(n, control) {
     let a = {
-        "w" : "+1",
-        "s" : "-1", 
-        "d" : "+10", 
-        "a" : "-10", 
+        "w" : +1,
+        "s" : -1, 
+        "d" : +10, 
+        "a" : -10, 
     }
     
     for(let i = 0; i <= control.length-1; i++) {
         n += a[control[i]];
     }
-    return eval(n);
+    return n;
 }
