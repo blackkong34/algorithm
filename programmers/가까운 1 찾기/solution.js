ae8c5aa5cb66892317d860fd519512b398c2c003
blackkong34 <107909808+blackkong34@users.noreply.github.com>
2023-04-25 23:24:01 +0900

    updated at 2023. 4. 25. 오후 11:24:02

--- programmers/가까운 1 찾기/solution.js
+++ programmers/가까운 1 찾기/solution.js
@@ -1,12 +1,13 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181898
 
 function solution(arr, idx) {
-    let answer = -1;
-    for (let i = idx; i <= arr.length-1; i++) {
-        if(arr[i]) {
-            answer = i;
-            break;
-        }
-    }
-    return answer;    
+//     let answer = -1;
+//     for (let i = idx; i <= arr.length-1; i++) {
+//         if(arr[i]) {
+//             answer = i;
+//             break;
+//         }
+//     }
+//     return answer; 
+    return arr.indexOf(1, idx);
 }
