f8ca96e8aa3be3a56da60114d8d9e7f52718d9ea
blackkong34 <107909808+blackkong34@users.noreply.github.com>
2023-05-01 14:04:52 +0900

    uploaded at 2023. 5. 1. 오후 2:04:52

---
+++ programmers/특수문자 출력하기/solution.js
@@ -0,0 +1,12 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181948
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('close', function () {
+    console.log(`!@#$%^&*(\\'"<>?:;`);
+});
+
