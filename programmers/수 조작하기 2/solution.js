b239068dc9ee5780600c36a206e39b1a039c91dd
blackkong34 <107909808+blackkong34@users.noreply.github.com>
2023-05-03 15:38:24 +0900

    updated at 2023. 5. 3. 오후 3:38:25

--- programmers/수 조작하기 2/solution.js
+++ programmers/수 조작하기 2/solution.js
@@ -3,8 +3,11 @@
 function solution(numLog) {
     var answer = "";
     const log = {1 : "w", "-1" : "s", 10 : "d","-10" : "a",};
-    for(let i = 0; i < numLog.length-1; i++) {
-        answer += log[(numLog[i+1] - numLog[i])];
-    }
-    return answer;
+    // for(let i = 0; i < numLog.length-1; i++) {
+    //     answer += log[(numLog[i+1] - numLog[i])];
+    // }
+    // return answer;
+    
+    return numLog.reduce((a, c, i) => i ? a + log[c - numLog[i-1]] : a,"");
+   
 }
