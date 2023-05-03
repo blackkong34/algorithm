808d3588b9e728c4da9115ec5537f42b8dadc7fc
blackkong34 <107909808+blackkong34@users.noreply.github.com>
2023-05-03 15:28:49 +0900

    uploaded at 2023. 5. 3. 오후 3:28:49

---
+++ programmers/수 조작하기 2/solution.js
@@ -0,0 +1,10 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181925
+
+function solution(numLog) {
+    var answer = "";
+    const log = {"1" : "w", "-1" : "s", "10" : "d","-10" : "a",};
+    for(let i = 0; i < numLog.length-1; i++) {
+        answer += log[(numLog[i+1] - numLog[i]) + ""];
+    }
+    return answer;
+}
