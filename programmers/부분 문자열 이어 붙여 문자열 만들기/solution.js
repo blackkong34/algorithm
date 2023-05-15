f7012a732395a65327d6c3093f1d5f5326736998
blackkong34 <107909808+blackkong34@users.noreply.github.com>
2023-05-15 17:34:54 +0900

    updated at 2023. 5. 15. 오후 5:34:54

--- programmers/부분 문자열 이어 붙여 문자열 만들기/solution.js
+++ programmers/부분 문자열 이어 붙여 문자열 만들기/solution.js
@@ -3,3 +3,9 @@
 function solution(my_strings, parts) {
     return parts.map((v, i) => my_strings[i].substring(v[0], v[1]+1)).join("")
 }
+
+    // let answer = '';
+    // for(let i = 0; i <= parts.length-1; i++) {
+    //    answer += my_strings[i].substring(parts[i][0], parts[i][1]+1)
+    // }
+    // return answer;
