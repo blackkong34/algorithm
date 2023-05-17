b72a2b9d19e16572f578fbfcbd2affc777ac9658
blackkong34 <107909808+blackkong34@users.noreply.github.com>
2023-05-17 17:45:18 +0900

    updated at 2023. 5. 17. 오후 5:45:18

--- programmers/홀짝에 따라 다른 값 반환하기/solution.js
+++ programmers/홀짝에 따라 다른 값 반환하기/solution.js
@@ -1,10 +1,8 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181935
 
 function solution(n) {
-    let answer = 0;
-    let i = n % 2;
-    
-    for(; i <= n; i += 2) {
+    let answer = 0;    
+    for(let i = n % 2; i <= n; i += 2) {
         answer += i % 2 ? i : i**2;
     }
 
