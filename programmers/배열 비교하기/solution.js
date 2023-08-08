e900c622abd16ceacda875548132094abfd6bc48
blackkong34 <107909808+blackkong34@users.noreply.github.com>
2023-08-08 14:37:55 +0900

    uploaded at 2023. 8. 8. 오후 2:37:57

---
+++ programmers/배열 비교하기/solution.js
@@ -0,0 +1,13 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181856?language=javascript
+
+function solution(arr1, arr2) {
+const compare = (arr1, arr2) => {
+    if(arr1.length === arr2.length) {
+        const ans1 = arr1.reduce((acc, cur) => acc + cur,0) 
+        const ans2 = arr2.reduce((acc, cur) => acc + cur,0) 
+    return ans1 > ans2 ? 1 : (ans1 < ans2) ? -1 : 0
+    } 
+   return arr1.length > arr2.length ? 1 : -1  
+}
+return compare(arr1, arr2);
+}
